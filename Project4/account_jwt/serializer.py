1f2827f0e65062713bef346f3cf73568b6d81781
Sourcery AI <>
2022-06-09 11:07:01 +0000

    'Refactored by Sourcery'

--- Project4/account_jwt/serializer.py
+++ Project4/account_jwt/serializer.py
@@ -13,8 +13,12 @@ class RegisterSerializer(serializers.ModelSerializer):
             'password':{'write_only': True},
         }
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'],     password = validated_data['password']  ,first_name=validated_data['first_name'],  last_name=validated_data['last_name'])
-        return user
+        return User.objects.create_user(
+            validated_data['username'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
 # User serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
